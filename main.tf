969583c41bbe2907159e37208ff0e84443f38703
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2022-11-03 15:55:41 +0100

    gitops workflow

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "mongodbatlas_project_ip_access_list" "ip" {
   cidr_block = var.cidr  
   comment    = "IP Address for accessing the cluster"
 }
-resource "mongodbatlas_cluster" "cluster" {
+resource "mongodbatlas_cluster" "cluster1" {
   project_id             = mongodbatlas_project.project.id
   name                   = var.cluster_name
   mongo_db_major_version = var.mongodbversion
