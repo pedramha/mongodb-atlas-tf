bb4fc7a2115f0ac6bf932ea12e24e71d38074698
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2023-07-24 11:20:04 +0200

    amend

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "mongodbatlas_project_ip_access_list" "ip" {
   project_id = mongodbatlas_project.project.id
   # ip_address = var.ip_address
   cidr_block = var.cidr  
-  comment    = "IP Address for accessing the cluster"
+  comment    = "The IP Address for accessing the cluster"
 }
 resource "mongodbatlas_cluster" "cluster" {
   project_id             = mongodbatlas_project.project.id
